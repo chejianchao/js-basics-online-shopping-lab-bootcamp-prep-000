92dfb3d355a665693f734e3b3a01c689449d8ad9
chejianchao <820992376@qq.com>
2018-06-14 03:52:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,11 +11,22 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
- cart.push(item)
+ cart.push({ itemName:item, itemPrice: Math.floor(Math.random()*100+1) })
+ return item
 }
 
 function viewCart() {
   // write your code here
+  var str = "In your cart, you have";
+  var idx = 0;
+  for( var idx = 0; idx<cart.length; idx++ ){
+    if( idx != 0 )
+      str += ','
+    if( idx  == cart.length-1 && cart.length>1)
+      str += " and"
+    str += ` ${item.itemName} at $${item.itemPrice}`
+  }
+  bananas at $17, pancake batter at $5, and eggs at $49
 }
 
 function total() {
